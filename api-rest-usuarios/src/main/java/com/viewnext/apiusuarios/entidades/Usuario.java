64943bbe02932f28d2803e5bfd0fbd9645e9e9ef
Miguel jmartinez <jcruz_13@hotmail.es>
2019-12-16 15:55:11 +0100

    USUARIO CLASE MODIFICADA

--- api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
+++ api-rest-usuarios/src/main/java/com/viewnext/apiusuarios/entidades/Usuario.java
@@ -8,10 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
-
-import org.springframework.lang.NonNull;
 
 @Entity
 public class Usuario implements Serializable{
@@ -20,11 +19,11 @@ public class Usuario implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@NonNull
+	@NotNull
 	@Size(min = 1, max = 50)
 	private String nombre;
 	
-	@NonNull
+	@NotNull
 	@Size(min = 3, max = 255)
 	@Pattern(regexp = "^[a-zA-z0-9._%+-]+@[a-zA-z0-9.-]+\\.[a-zA-Z]{2,6}$")
 	private String email;
@@ -78,6 +77,4 @@ public class Usuario implements Serializable{
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
-
 }
