cc1d3addffe78dc7718d6aa3a170d576b7d34568
Miguel jmartinez <jcruz_13@hotmail.es>
2019-12-20 17:21:12 +0100

    ultima version front a
    20/12/2019

---
+++ app-front-end/src/app/entidades/Usuario.ts
@@ -0,0 +1,7 @@
+export class Usuario {
+    public id: number;
+    public nombre: string
+    public email: string;
+    public password: string;
+    public idTemaPreferido: number;
+}
---
+++ app-front-end/src/app/usuarios.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import { Usuario } from './entidades/Usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuariosService {
+  listaUsuario: Usuario[];
+  constructor() { }
+}
